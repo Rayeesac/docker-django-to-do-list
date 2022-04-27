d6cbd2fefae5b6e7e60bac1ac44c52536078aeb9
Rayees AC <acr9030@gmail.com>
2022-04-27 19:21:38 +0530

    Update settings.py

--- django/todo_list/settings.py
+++ django/todo_list/settings.py
@@ -145,11 +145,3 @@ STATICFILES_DIRS = [
 ]
 
 LOGIN_URL = 'login'
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'info.rayeesac@gmail.com'
-EMAIL_HOST_PASSWORD = 'gojwbgyvcdkgpnrh'
